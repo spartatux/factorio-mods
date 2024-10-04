c20dfc968abacf6c6eaef88c03d77421f4e0139f
Luzifer Senpai <luzifer@luzifersenpai.de>
2024-10-04 22:08:04 +0200

    Added research-complete

---
+++ research-complete/data-updates.lua
@@ -0,0 +1,2 @@
+data.raw["utility-sounds"]["default"]["research_completed"].filename = "__research-complete__/research-complete.ogg"
+data.raw["utility-sounds"]["default"]["research_completed"].volume = 2
